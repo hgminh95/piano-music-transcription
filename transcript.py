b00014b20619cc21e9fcb30e702ddba15e9a1b8d
hgminh <hgminh95@gmail.com>
2017-02-05 12:41:11 +0700

    Get transcriber using method name

--- transcript.py
+++ transcript.py
@@ -1,10 +1,43 @@
-import argparse
+# -*- coding: utf-8 -*-
 
+import argparse
+import logging
+
+from core.transcriber import MetaTranscriber
 import transcribers
+
+logging.basicConfig(level=logging.DEBUG,
+                    format="%(lineno)d in %(filename)s: %(message)s")
+
+_logger = logging.getLogger(__name__)
+
+
+def do_transcribe(args):
+    for name, transcribers in MetaTranscriber.method_to_transcriber.iteritems():
+        if name == args.method:
+            _logger.info(name)
 
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Piano Transcription')
+    subparsers = parser.add_subparsers(help='commands')
+
+    # Parser for transcribe command
+    transcribe_parser = subparsers.add_parser('transcribe',
+        help='Transcribe audio music to midi file')
+    transcribe_parser.add_argument('-m', '--method',
+        help='method used to transcribe music',
+        default='dummy')
+    transcribe_parser.add_argument('-i', '--input',
+        help='input file or folder')
+    transcribe_parser.add_argument('-o', '--output',
+        help='output file or folder')
+    transcribe_parser.set_defaults(sub='transcribe')
 
     args = parser.parse_args()
+
+    if args.sub == 'transcribe':
+        do_transcribe(args)
+    else:
+        pass
