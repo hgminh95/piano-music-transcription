a774b58ee0763d2853fd54b4fde4edbb05b209d7
hgminh <hgminh95@gmail.com>
2017-02-12 11:14:07 +0700

    Leftjoin now merge event happens at same time

--- evaluation/algo.py
+++ evaluation/algo.py
@@ -19,16 +19,14 @@ def leftjoin(ans, expect, window=0.05):
     expect_len = len(expect)
 
     i, j = 0, 0
-    taken = False
+    notes = []
     while i < ans_len and j < expect_len:
         if abs(ans[i][0] - expect[j][0]) <= window:
-            yield ans[i][0], expect[j][2]
-            taken = True
+            notes.append(expect[j][2])
             j = incif(j, expect_len)
         elif ans[i] > expect[j]:
             j = incif(j, expect_len)
         else:
-            if not taken:
-                yield ans[i][0], -1
+            yield ans[i][0], notes
             i = incif(i, ans_len)
-            taken = False
+            notes = []
