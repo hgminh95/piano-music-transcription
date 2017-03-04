facad3df3c4770d76871f7c2a6b1b15458b1fd55
hgminh <hgminh95@gmail.com>
2017-03-04 17:57:24 +0700

    Add test script

---
+++ test.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+source activate tensorflow
+
+python transcript.py construct -i . -o test.data
+python transcript.py train -i test.data -o test.pkl
+python transcript.py eval -mf test.pkl -i .
